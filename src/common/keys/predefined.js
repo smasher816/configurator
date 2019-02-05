57d0252532dfbc67b3de59fefb555a24424e57f2
Rowan Decker <Smasher816@gmail.com>
2019-02-05 12:33:09 -0800

    Add gamma control keys

--- src/common/keys/predefined.js
+++ src/common/keys/predefined.js
@@ -243,6 +243,10 @@ function buildKeys() {
     ['vis/clear', 'V:CLEAR', ['#:animation_control(7)'], c.vis],
     ['vis/spd+', 'V:SPD+',   ['#:ledControl(7, 1)'], c.vis],
     ['vis/spd-', 'V:SPD-',   ['#:ledControl(8, 1)'], c.vis],
+    ['gamma/tog', 'GAMMA', ['#:gamma(2)'], c.vis],
+    ['gamma/on', 'GAM ON',   ['#:gamma(1)'], c.vis, { fontSize: 11 }],
+    ['gamma/off', 'GAM OFF', ['#:gamma(0)'], c.vis, { fontSize: 11 }],
+    //TODO: fade_set()
 
     // i11n
     ['i11l/iso#', 'ISO#', ['HASH', 'NUMBER', '#'], c.i11l],
